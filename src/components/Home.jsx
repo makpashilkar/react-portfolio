751aff6609a6b114a2a617adfe34e6938a306291
makpashilkar <makpashilkar@gmail.com>
2023-10-24 00:30:48 +0530

    minor refactor

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -11,15 +11,15 @@ const Home = () => {
     >
       <div className="max-w-screen-lg mx-auto flex flex-col items-center justify-center h-full px-4 md:flex-row">
         <div className="flex flex-col justify-center h-full">
-          <h2 className="text-4xl sm:text-7xl font-bold text-white">
+          <h2 className="text-4xl sm:text-7xl font-bold text-white sm:mt-5">
             I'm a Web Developer
           </h2>
           <p className="text-gray-500 py-4 max-w-md">
             Courteous and enthusiastic. I am interested in IT and everything in
-            its orbit. I am fascinated by web programming,
-            e.g. developing apps and building websites. I am keen to gain more
-            experience in the field. For this reason, I am looking to join an
-            organisation that is in alignment with my goals.
+            its orbit. I am fascinated by web programming, e.g. developing apps
+            and building websites. I am keen to gain more experience in the
+            field. For this reason, I am looking to join an organisation that is
+            in alignment with my goals.
           </p>
 
           <div>
